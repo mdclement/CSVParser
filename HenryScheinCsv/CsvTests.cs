b2155d295df32dc42313f0d89babcc250ec4be5b
Mike Clement <mike@softwareontheside.com>
2011-10-06 23:32:54 -0600

    first commit

---
+++ HenryScheinCsv/CsvTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace HenryScheinCsv
+{
+    [TestFixture]
+    public class CsvTests
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            var parser = new CsvParser();
+            var parsed = parser.Parse(string.Empty);
+            Assert.That(parsed.Count(), Is.EqualTo(1));
+            Assert.That(parsed[0], Is.EqualTo(string.Empty));
+        }
+    }
+
+    public class CsvParser
+    {
+        public IList<string> Parse(string stringToParse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
